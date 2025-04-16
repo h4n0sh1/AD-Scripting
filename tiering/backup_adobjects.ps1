212ebf678dd7f0f86171444121378ee86375d459
h4n0sh1 <hanchi.oth@gmail.com>
2025-04-16 13:04:25 +0200

    adds Tiering backup script

---
+++ tiering/backup_adobjects.ps1
@@ -0,0 +1,16 @@
+# Extact from MISC 137 // J.Khalfa
+
+# Script de backup des objets AD
+Get-ADOrganizationalUnit -Filter * -Properties DistinguishedName | Select-Object Name, DistinguishedName | Export-Csv -Path "OUExport.csv" -NoTypeInformation
+Get-ADUser -Filter * -Properties DistinguishedName | Select-Object Name, DistinguishedName | Export-Csv -Path "UserExport.csv" -NoTypeInformation
+Get-ADGroup -Filter * -Properties DistinguishedName | Select-Object Name, DistinguishedName | Export-Csv -Path "GroupExport.csv" -NoTypeInformation
+Get-ADComputer -Filter * -Properties DistinguishedName | Select-Object Name, DistinguishedName | Export-Csv -Path "ComputerExport.csv" -NoTypeInformation
+
+#Script de backup des objets GPO
+Get-GPO -All | Export-Csv -Path "GPOExport.csv" -NoTypeInformation 
+Get-GPOReport -All -ReportType XML -Path "GPOReport.xml"
+Get-GPOReport -All -ReportType HTML -Path "GPOReport.html"
+$gpos = Get-ADOrganizationUnit -Filter * | Get-GPInheriteance | Select-Object -ExpandProperty GpoLinks
+$gpos | Export-Csv -Path "GPO_linked.csv" -NoTypeInformation
+$gpos_root_domain = Get-AdDomain | Get-GPInheritance
+$gpos_root_domain | Export-Csv GPO_linked_root_domain.csv -NoTypeInformation
