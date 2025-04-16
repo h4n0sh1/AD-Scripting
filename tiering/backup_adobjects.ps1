5b740d333e94381468ccf5dc83c12c02dd26d3ad
h4n0sh1 <hanchi.oth@gmail.com>
2025-04-16 13:14:32 +0200

    corrects Typo

--- tiering/backup_adobjects.ps1
+++ tiering/backup_adobjects.ps1
@@ -10,7 +10,7 @@ Get-ADComputer -Filter * -Properties DistinguishedName | Select-Object Name, Dis
 Get-GPO -All | Export-Csv -Path "GPOExport.csv" -NoTypeInformation 
 Get-GPOReport -All -ReportType XML -Path "GPOReport.xml"
 Get-GPOReport -All -ReportType HTML -Path "GPOReport.html"
-$gpos = Get-ADOrganizationUnit -Filter * | Get-GPInheriteance | Select-Object -ExpandProperty GpoLinks
+$gpos = Get-ADOrganizationalUnit -Filter * | Get-GPInheriteance | Select-Object -ExpandProperty GpoLinks
 $gpos | Export-Csv -Path "GPO_linked.csv" -NoTypeInformation
 $gpos_root_domain = Get-AdDomain | Get-GPInheritance
 $gpos_root_domain | Export-Csv GPO_linked_root_domain.csv -NoTypeInformation
